c780817a15c00462a487ce26533ca39ab84b101c
deprekate <deprekate@gmail.com>
2022-02-18 17:01:02 -0800

    Pushing some code

---
+++ slip.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL) 
+import os
+import sys
+import argparse
+from argparse import RawTextHelpFormatter
+from subprocess import Popen, PIPE, STDOUT
+from types import MethodType
+
+#sys.path.pop(0)
+import slippery
+from slippery.file import File
+from slippery.functions import *
+
+def extra(self, value=None):
+	return 'extra'
+
+def is_valid_file(x):
+	if not os.path.exists(x):
+		raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+	return x
+
+
+if __name__ == '__main__':
+	usage = '%s [-opt1, [-opt2, ...]] infile' % __file__
+	parser = argparse.ArgumentParser(description='', formatter_class=RawTextHelpFormatter, usage=usage)
+	parser.add_argument('infile', type=is_valid_file, help='input file')
+	parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
+	args = parser.parse_args()
+
+	fp = File(args.infile)
+	for name,locus in fp.items():
+		#locus.rare_codons = MethodType(rare_codons, locus)
+		#for feature in locus:
+		#	print(feature, feature.more())
+		#print(locus.rare_codons())
+		locus.write(args.outfile)
+	
+
